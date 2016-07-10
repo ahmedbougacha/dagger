03ae06b4012969113770a2a9cfc698c7fac78267
Ahmed Bougacha <ahmed.bougacha@gmail.com>
2016-07-10 00:34:42 -0700

    [DC][X86] Add PUSHF/POPF sema patterns.

--- test/DC/X86/Instructions/popf_popfd_popfq.s
+++ test/DC/X86/Instructions/popf_popfd_popfq.s
@@ -8,8 +8,17 @@ popfw
 
 ## POPF64
 # CHECK-LABEL: call void @llvm.dc.startinst
-# CHECK-NEXT: call void @llvm.trap()
-# CHECK-NEXT: unreachable
+# CHECK-NEXT: [[RIP_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RIP")
+# CHECK-NEXT: [[V0:%.+]] = add i64 [[RIP_0]], 1
+# CHECK-NEXT: call void @llvm.dc.setreg{{.*}} !"RIP")
+# CHECK-NEXT: [[RSP_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RSP")
+# CHECK-NEXT: [[V1:%.+]] = add i64 [[RSP_0]], 4
+# CHECK-NEXT: call void @llvm.dc.setreg.i64(i64 [[V1]], metadata !"RSP")
+# CHECK-NEXT: [[RSP_1:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RSP")
+# CHECK-NEXT: [[V2:%.+]] = sub i64 [[RSP_1]], 4
+# CHECK-NEXT: [[V3:%.+]] = inttoptr i64 [[V2]] to i32*
+# CHECK-NEXT: [[V4:%.+]] = load i32, i32* [[V3]], align 1
+# CHECK-NEXT: call void @llvm.dc.setreg.i32(i32 [[V4]], metadata !"EFLAGS")
 popfq
 
 retq
--- test/DC/X86/Instructions/pushf_pushfd.s
+++ test/DC/X86/Instructions/pushf_pushfd.s
@@ -8,8 +8,17 @@ pushfw
 
 ## PUSHF64
 # CHECK-LABEL: call void @llvm.dc.startinst
-# CHECK-NEXT: call void @llvm.trap()
-# CHECK-NEXT: unreachable
+# CHECK-NEXT: [[RIP_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RIP")
+# CHECK-NEXT: [[V0:%.+]] = add i64 [[RIP_0]], 1
+# CHECK-NEXT: call void @llvm.dc.setreg{{.*}} !"RIP")
+# CHECK-NEXT: [[EFLAGS_0:%.+]] = call i32 @llvm.dc.getreg.i32(metadata !"EFLAGS")
+# CHECK-NEXT: [[RSP_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RSP")
+# CHECK-NEXT: [[V1:%.+]] = sub i64 [[RSP_0]], 4
+# CHECK-NEXT: [[V2:%.+]] = inttoptr i64 [[V1]] to i32*
+# CHECK-NEXT: store i32 [[EFLAGS_0]], i32* [[V2]], align 1
+# CHECK-NEXT: [[RSP_1:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RSP")
+# CHECK-NEXT: [[V3:%.+]] = sub i64 [[RSP_1]], 4
+# CHECK-NEXT: call void @llvm.dc.setreg.i64(i64 [[V3]], metadata !"RSP")
 pushfq
 
 retq
