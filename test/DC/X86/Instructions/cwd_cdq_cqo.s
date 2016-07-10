c14de2d4cbd4cada3123f5dd798b18ccc6ce7439
Ahmed Bougacha <ahmed.bougacha@gmail.com>
2016-07-10 00:35:29 -0700

    [DC][X86] Explicitly copy CQO RAX imp-def/use.
    Thanks to the smarter (implicit) generation, we end up with
    an (implicit) for RAX that we can't handle. Fix it.

--- test/DC/X86/Instructions/cwd_cdq_cqo.s
+++ test/DC/X86/Instructions/cwd_cdq_cqo.s
@@ -1,15 +1,59 @@
 # RUN: llvm-mc -filetype=obj -o - %s | llvm-dec - -dc-translate-unknown-to-undef -enable-dc-reg-mock-intrin | FileCheck %s
 
-# XFAIL: *
-
 ## CDQ
+# CHECK-LABEL: call void @llvm.dc.startinst
+# CHECK-NEXT: [[RIP_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RIP")
+# CHECK-NEXT: [[V0:%.+]] = add i64 [[RIP_0]], 1
+# CHECK-NEXT: call void @llvm.dc.setreg{{.*}} !"RIP")
+# CHECK-NEXT: [[EAX_0:%.+]] = call i32 @llvm.dc.getreg.i32(metadata !"EAX")
+# CHECK-NEXT: [[V1:%.+]] = ashr i32 [[EAX_0]], 31
+# CHECK-NEXT: call void @llvm.dc.setreg.i32(i32 [[V1]], metadata !"EDX")
+# CHECK-NEXT: [[EAX_1:%.+]] = call i32 @llvm.dc.getreg.i32(metadata !"EAX")
+# CHECK-NEXT: call void @llvm.dc.setreg.i32(i32 [[EAX_1]], metadata !"EAX")
 cltd
+
 ## CDQE
+# CHECK-LABEL: call void @llvm.dc.startinst
+# CHECK-NEXT: [[RIP_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RIP")
+# CHECK-NEXT: [[V0:%.+]] = add i64 [[RIP_0]], 2
+# CHECK-NEXT: call void @llvm.dc.setreg{{.*}} !"RIP")
+# CHECK-NEXT: [[EAX_0:%.+]] = call i32 @llvm.dc.getreg.i32(metadata !"EAX")
+# CHECK-NEXT: [[V1:%.+]] = sext i32 [[EAX_0]] to i64
+# CHECK-NEXT: call void @llvm.dc.setreg.i64(i64 [[V1]], metadata !"RAX")
 cltq
+
 ## CQO
+# CHECK-LABEL: call void @llvm.dc.startinst
+# CHECK-NEXT: [[RIP_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RIP")
+# CHECK-NEXT: [[V0:%.+]] = add i64 [[RIP_0]], 2
+# CHECK-NEXT: call void @llvm.dc.setreg{{.*}} !"RIP")
+# CHECK-NEXT: [[RAX_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RAX")
+# CHECK-NEXT: [[V1:%.+]] = ashr i64 [[RAX_0]], 63
+# CHECK-NEXT: call void @llvm.dc.setreg.i64(i64 [[V1]], metadata !"RDX")
+# CHECK-NEXT: [[RAX_1:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RAX")
+# CHECK-NEXT: call void @llvm.dc.setreg.i64(i64 [[RAX_1]], metadata !"RAX")
 cqto
+
 ## CWD
+# CHECK-LABEL: call void @llvm.dc.startinst
+# CHECK-NEXT: [[RIP_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RIP")
+# CHECK-NEXT: [[V0:%.+]] = add i64 [[RIP_0]], 2
+# CHECK-NEXT: call void @llvm.dc.setreg{{.*}} !"RIP")
+# CHECK-NEXT: [[AX_0:%.+]] = call i16 @llvm.dc.getreg.i16(metadata !"AX")
+# CHECK-NEXT: [[V1:%.+]] = ashr i16 [[AX_0]], 15
+# CHECK-NEXT: call void @llvm.dc.setreg.i16(i16 [[V1]], metadata !"DX")
+# CHECK-NEXT: [[AX_1:%.+]] = call i16 @llvm.dc.getreg.i16(metadata !"AX")
+# CHECK-NEXT: call void @llvm.dc.setreg.i16(i16 [[AX_1]], metadata !"AX")
 cwtd
+
 ## CWDE
+# CHECK-LABEL: call void @llvm.dc.startinst
+# CHECK-NEXT: [[RIP_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RIP")
+# CHECK-NEXT: [[V0:%.+]] = add i64 [[RIP_0]], 1
+# CHECK-NEXT: call void @llvm.dc.setreg{{.*}} !"RIP")
+# CHECK-NEXT: [[AX_0:%.+]] = call i16 @llvm.dc.getreg.i16(metadata !"AX")
+# CHECK-NEXT: [[V1:%.+]] = sext i16 [[AX_0]] to i32
+# CHECK-NEXT: call void @llvm.dc.setreg.i32(i32 [[V1]], metadata !"EAX")
 cwtl
+
 retq
