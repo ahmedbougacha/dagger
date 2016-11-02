0198264f0c4cdf3945651695f28371684079e53c
Ahmed Bougacha <ahmed.bougacha@gmail.com>
2016-11-02 08:26:56 -0700

    [DC][DYN] Remove MemoryObject.h #include. NFC.
    We don't use MemoryObject anymore, and it was removed in r285774.

--- tools/dyn/dyncore/dyncore.cpp
+++ tools/dyn/dyncore/dyncore.cpp
@@ -38,7 +38,6 @@
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/MemoryBuffer.h"
-#include "llvm/Support/MemoryObject.h"
 #include "llvm/Support/PrettyStackTrace.h"
 #include "llvm/Support/Signals.h"
 #include "llvm/Support/TargetRegistry.h"
