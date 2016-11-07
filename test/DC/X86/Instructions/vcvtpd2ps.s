3cffc07978fbb0bb500ce031f807eca253327bbf
Ahmed Bougacha <ahmed.bougacha@gmail.com>
2016-11-07 06:29:11 -0800

    [DC][X86] Update vcvtpd2ps tests for upstream change (r286057).
    We now use FP_ROUND instead of intrinsics.

--- test/DC/X86/Instructions/vcvtpd2ps.s
+++ test/DC/X86/Instructions/vcvtpd2ps.s
@@ -2,14 +2,32 @@
 
 ## VCVTPD2PSYrm
 # CHECK-LABEL: call void @llvm.dc.startinst
-# CHECK-NEXT: call void @llvm.trap()
-# CHECK-NEXT: unreachable
+# CHECK-NEXT: [[RIP_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RIP")
+# CHECK-NEXT: [[V0:%.+]] = add i64 [[RIP_0]], 7
+# CHECK-NEXT: call void @llvm.dc.setreg{{.*}} !"RIP")
+# CHECK-NEXT: [[RBX_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RBX")
+# CHECK-NEXT: [[R14_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"R14")
+# CHECK-NEXT: [[V1:%.+]] = mul i64 [[R14_0]], 2
+# CHECK-NEXT: [[V2:%.+]] = add i64 [[V1]], 2
+# CHECK-NEXT: [[V3:%.+]] = add i64 [[RBX_0]], [[V2]]
+# CHECK-NEXT: [[V4:%.+]] = inttoptr i64 [[V3]] to <4 x double>*
+# CHECK-NEXT: [[V5:%.+]] = load <4 x double>, <4 x double>* [[V4]], align 1
+# CHECK-NEXT: [[V6:%.+]] = fptrunc <4 x double> [[V5]] to <4 x float>
+# CHECK-NEXT: [[V7:%.+]] = bitcast <4 x float> [[V6]] to i128
+# CHECK-NEXT: call void @llvm.dc.setreg.i128(i128 [[V7]], metadata !"XMM8")
 vcvtpd2psy	2(%rbx,%r14,2), %xmm8
 
 ## VCVTPD2PSYrr
 # CHECK-LABEL: call void @llvm.dc.startinst
-# CHECK-NEXT: call void @llvm.trap()
-# CHECK-NEXT: unreachable
+# CHECK-NEXT: [[RIP_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"RIP")
+# CHECK-NEXT: [[V0:%.+]] = add i64 [[RIP_0]], 5
+# CHECK-NEXT: call void @llvm.dc.setreg{{.*}} !"RIP")
+# CHECK-NEXT: [[YMM9_0:%.+]] = call <8 x float> @llvm.dc.getreg.v8f32(metadata !"YMM9")
+# CHECK-NEXT: [[V1:%.+]] = bitcast <8 x float> [[YMM9_0]] to i256
+# CHECK-NEXT: [[V2:%.+]] = bitcast i256 [[V1]] to <4 x double>
+# CHECK-NEXT: [[V3:%.+]] = fptrunc <4 x double> [[V2]] to <4 x float>
+# CHECK-NEXT: [[V4:%.+]] = bitcast <4 x float> [[V3]] to i128
+# CHECK-NEXT: call void @llvm.dc.setreg.i128(i128 [[V4]], metadata !"XMM8")
 vcvtpd2psy	%ymm9, %xmm8
 
 ## VCVTPD2PSrr
