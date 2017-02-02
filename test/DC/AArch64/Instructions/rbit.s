84dd615d62622bb87768bbcad4c5871d997bf27b
Ahmed Bougacha <ahmed.bougacha@gmail.com>
2017-02-01 23:37:10 -0800

    [DC][AArch64] Update RBIT tests for upstream change (r291575).
    These actually have generic semantics now, as the @llvm.aarch64.rbit
    intrinsic they used to map to has been replaced with @llvm.bitreverse.

--- test/DC/AArch64/Instructions/rbit.s
+++ test/DC/AArch64/Instructions/rbit.s
@@ -2,12 +2,20 @@
 
 ;; RBITWr
 ; CHECK-LABEL: call void @llvm.dc.startinst
+; CHECK-NEXT: [[PC_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"PC")
+; CHECK-NEXT: [[V0:%.+]] = add i64 [[PC_0]], 4
+; CHECK-NEXT: call void @llvm.dc.setreg{{.*}} !"PC")
+; CHECK-NEXT: [[W17_0:%.+]] = call i32 @llvm.dc.getreg.i32(metadata !"W17")
 ; CHECK-NEXT: call void @llvm.trap()
 ; CHECK-NEXT: unreachable
 rbit	w16, w17
 
 ;; RBITXr
 ; CHECK-LABEL: call void @llvm.dc.startinst
+; CHECK-NEXT: [[PC_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"PC")
+; CHECK-NEXT: [[V0:%.+]] = add i64 [[PC_0]], 4
+; CHECK-NEXT: call void @llvm.dc.setreg{{.*}} !"PC")
+; CHECK-NEXT: [[X17_0:%.+]] = call i64 @llvm.dc.getreg.i64(metadata !"X17")
 ; CHECK-NEXT: call void @llvm.trap()
 ; CHECK-NEXT: unreachable
 rbit	x16, x17
