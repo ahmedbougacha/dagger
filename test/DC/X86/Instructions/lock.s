895ccaa4f7ad515e0f117a9886c46416a8358311
Ahmed Bougacha <ahmed.bougacha@gmail.com>
2017-02-15 20:33:53 -0800

    [DC][X86] Remove tests for prefix opcodes. NFC.
    It doesn't make much sense to test these in isolation.
    The fact that they have their own MC opcode is a terrible hack that
    shouldn't be necessary in the first place.

--- test/DC/X86/Instructions/lock.s
+++
@@ -1,9 +0,0 @@
-# RUN: llvm-mc -triple x86_64--darwin -filetype=obj -o - %s | llvm-dec - -dc-translate-unknown-to-undef -enable-dc-reg-mock-intrin | FileCheck %s
-
-# XFAIL: *
-
-# CHECK: @llvm.dc.startinst
-
-## LOCK_PREFIX
-lock
-retq
